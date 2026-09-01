17bf74453e738c9ffcc1209ec0db24f2bfc94c60
arjun939201 <throwuse829@gmail.com>
2026-09-01 18:20:06 +0530

    Create intro.jsx

---
+++ intro.jsx
@@ -0,0 +1,13 @@
+function App() {
+  const name = "Arjun";
+  const age = 23;
+
+  return (
+    <>
+      <h1>Myself {name}, {age} years old</h1>
+      <h2>I'm learning ReactJS</h2>
+    </>
+  );
+}
+
+export default App;
